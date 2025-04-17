6c1143549fbae8ba82ea1f73ef6bd4d71b268096
Kamsiyochukwu Arinze <arinze_kamsiyochukwu@outlook.com>
2025-04-17 14:34:08 -0400

    removed unused import from hook

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,7 +1,7 @@
 import { GameQuery } from "@/App"
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query"
-import { Platform } from "./usePlatforms"
 import APIClient, { FetchResponse } from '@/services/api-client'
+import { useInfiniteQuery } from "@tanstack/react-query"
+import { Platform } from "./usePlatforms"
 
 const apiClient = new APIClient<Game>('/games')
 
